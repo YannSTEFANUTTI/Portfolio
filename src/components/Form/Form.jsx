8070323d46c704e1c61ebe090b5cc03ef1b8440c
YannSTEFANUTTI <stefanutti.yann@gmail.com>
2023-02-28 16:53:06 +0100

    form fixed 01

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,36 +1,37 @@
-import "./form.css"
+import "./form.css";
 const Form = () => {
+  return (
+    <div className="formPage">
+      <form
+        className="formular"
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        onSubmit="submit"
+      >
+        <h1>Contactez-moi</h1>
+        <p>
+          <input type="hidden" name="form-name" value="contact"></input>
+          <label>
+            Nom <br /> <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email <br /> <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message <br /> <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Envoyer</button>
+        </p>
+      </form>
+    </div>
+  );
+};
 
-    return (
-      <div className="formPage">
-        <form
-          className="formular"
-          name="contact"
-          method="POST"
-          data-netlify="true"
-        >
-          <h1>Contactez-moi</h1>
-          <p>
-            <label>
-              Nom <br /> <input type="text" name="name" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Email <br /> <input type="email" name="email" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message <br /> <textarea name="message"></textarea>
-            </label>
-          </p>
-          <p>
-            <button type="submit">Envoyer</button>
-          </p>
-        </form>
-      </div>
-    );
-  }
-
-export default Form
+export default Form;
