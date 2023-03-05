70a3c14f4e72021821979b132e7c11108d277317
YannSTEFANUTTI <stefanutti.yann@gmail.com>
2023-03-05 11:11:20 +0100

    navBar move done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,73 +1,49 @@
-import React from "react";
+import { useRef, useEffect } from "react";
 import gsap from "gsap";
+import { Draggable } from "gsap/Draggable";
+import "./navBar.css";
+import contact from "../../assets/img/contact.png";
+import home from "../../assets/img/home.png";
+import projet from "../../assets/img/projet.png";
+import move from "../../assets/img/move.png";
+
+gsap.registerPlugin(Draggable);
 
 const NavBar = () => {
-  const handleZoomIn = () => {
-    const TL = gsap.timeline();
-    let mm = gsap.matchMedia();
-  
-    mm.add("(min-width:801px) and (max-width: 1999px)", () => {
-      TL.to(".nameTitle", {
-        scale: 1.1,
-        duration: 0.5,
-        rotation: 0,
-        x: 0,
-        zIndex: 0,
-        ease: "back",
-      });
-      gsap.to(".EachCard5", {
-        scale: 1.15,
-        duration: 0.5,
-        rotation: 0,
-        x: -150,
-        zIndex: 5,
-        ease: "back",
-      });
-    });
-    mm.add("(min-width: 2000px)", () => {
-      TL.to(".nameTitle", {
-        scale: 1.3,
-        duration: 0.5,
-        rotation: 0,
-        x: 0,
-        zIndex: 0,
-        ease: "back",
-      });
-      gsap.to(".EachCard5", {
-        scale: 0.9,
-        duration: 0.5,
-        rotation: 0,
-        x: 40,
-        zIndex: 0,
-        ease: "back",
-      });
-    });
-  };
-  const handleZoomOut = () => {
-    let mm = gsap.matchMedia();
-    mm.add("(max-width: 1999px)", () => {
-      gsap.to(".nameTitle", {
-        scale: 1,
-        duration: 0.2,
-        rotation: 0,
-        x: 0,
-        zIndex: 0,
-      });
-    });
-    mm.add("(min-width: 2000px)", () => {
-      gsap.to(".nameTitle", {
-        scale: 1,
-        duration: 0.2,
-        rotation: 0,
-        x: 0,
-        zIndex: 0,
-      });
-    });
-  };
+  const navRef = useRef(null);
 
+  function handleClick() {
+    const { x, y, width, height } = navRef.current.getBoundingClientRect();
+    const centerX = x + width / 2;
+    const centerY = y + height / 2;
+    gsap.set(navRef.current, {
+      transformOrigin: `${centerX}px ${centerY}px`,
+    });
+  }
+
+  useEffect(() => {
+    gsap.set(navRef.current, {
+      x: "50%",
+      y: "50%",
+      transformOrigin: "center",
+    });
+    Draggable.create(navRef.current, {
+      type: "x,y",
+      edgeResistance: 0.65,
+      onPress: handleClick,
+      onDrag: function () {
+        const { x, y, width, height } = this.target.getBoundingClientRect();
+        const newCenterX = x + width / 2;
+        const newCenterY = y + height / 2;
+        gsap.set(this.target, {
+          transformOrigin: `${newCenterX}px ${newCenterY}px`,
+        });
+      },
+    });
+  }, []);
   return (
-    <div className="titleBar">
-      
+    <div ref={navRef} className="navBar">
+      <img src={move} alt="move" id="move" />
       <a
         className="nameTitle"
         onMouseEnter={() => handleZoomIn()}
@@ -76,8 +52,27 @@ const NavBar = () => {
         target="_blank"
         rel="noreferrer"
       >
-        <h1>NAVBAR</h1>
-        <h1 className="nameAnim">Yann Stefanutti</h1>
+        <img id="home" src={home} alt="home link" />
+      </a>
+      <a
+        className="nameTitle"
+        onMouseEnter={() => handleZoomIn()}
+        onMouseLeave={() => handleZoomOut()}
+        href={""}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={projet} alt="projet link" />
+      </a>
+      <a
+        className="nameTitle"
+        onMouseEnter={() => handleZoomIn()}
+        onMouseLeave={() => handleZoomOut()}
+        href={""}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={contact} alt="contact link" />
       </a>
     </div>
   );
